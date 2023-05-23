fc8409a42194fa9667476c74a8a2642824b71488
estebannahuel <estebanaugieri44@gmail.com>
2023-05-22 21:14:32 -0300

    modifique

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const sumar = require("./practicaequipo/sumar")
-const restar = require("./practicaequipo/restar")
-const multiplicar = require("./practicaequipo/multiplicar")
-const division  = require("./practicaequipo/division")
+const sumar = require("./sumar")
+const restar = require("./restar")
+const multiplicar = require("./multiplicar")
+const division  = require("./division")
 const argv = require("process")
 const operador = argv[2]
-let resultado = 0
+
 
 switch (operador){
 
     case "sumar":
+
+  
     
 
 
-    
 
+    console.log(sumar(argv[3],argv[4]));
 
 
 
