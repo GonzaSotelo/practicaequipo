b37ce2807150539af2a3765d5f29eb4bda8be997
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-05-23 19:09:32 -0300

    modificaciones

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const sumar = require("./sumar")
-const restar = require("./restar")
-const multiplicar = require("./multiplicar")
-const division  = require("./division")
-const argv = require("process")
-const operador = argv[2]
+const sumar = require("./sumar");
+const restar = require("./restar");
+const multiplicar = require("./multiplicar");
+const division = require("./division");
+const argv = require("process");
+const operador = argv[2];
+const a = Number(process.argv[3]);
+const b = Number(process.argv[4]);
 
-
-switch (operador){
-
-    case "sumar":
-
-  
-    
-
-
-
-    console.log(sumar(argv[3],argv[4]));
-
-
-
+switch (operador) {
+  case "sumar":
+    console.log(sumar(argv[3], argv[4]));
+    break;
+  case "restar":
+    console.log(restar(a, b));
+    break;
+  case "dividir":
+    console.log(dividir(a, b));
+    break;
+  case "multiplicar":
+    console.log(multiplicar(a, b));
+    break;
 }
