93e7114408d88de44d60f4591eb7479e02106ba3
estebannahuel <estebanaugieri44@gmail.com>
2023-05-22 21:11:14 -0300

    agrego app con variables declaradas

---
+++ app.js
@@ -0,0 +1,20 @@
+const sumar = require("./practicaequipo/sumar")
+const restar = require("./practicaequipo/restar")
+const multiplicar = require("./practicaequipo/multiplicar")
+const division  = require("./practicaequipo/division")
+const argv = require("process")
+const operador = argv[2]
+let resultado = 0
+
+switch (operador){
+
+    case "sumar":
+    
+
+
+    
+
+
+
+
+}
